7c3ef8c93436dc79f1d5efff93be21f27b81faef
npbassett <bassett.neil@gmail.com>
2025-01-20 11:28:34 -0700

    Initial Commit

---
+++ WeatherTracker/WeatherTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerApp.swift
+//  WeatherTracker
+//
+//  Created by Neil Bassett on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
